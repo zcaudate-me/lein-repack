8f2ea24b13331ea5deb9478279555f66715840d9
Chris Zheng <chris.zheng@auspost.com.au>
2014-07-23 16:40:12 +1000

    0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-repack "0.1.4"
+(defproject lein-repack "0.1.5-SNAPSHOT"
   :description "Repack your project for deployment"
   :url "https://www.github.com/zcaudate/lein-repack"
   :license {:name "The MIT License"
