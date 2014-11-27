02182103219c11d6f7fc4afdccb57e6117ed483c
Rick Moynihan <rick.m@swirrl.com>
2014-11-27 12:38:50 +0000

    change clojars group name so we can deploy temporary fork.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-repack "0.2.5"
+(defproject org.clojars.rickmoynihan/lein-repack "0.2.5-SNAPSHOT"
   :description "Repack your project for deployment"
   :url "https://www.github.com/zcaudate/lein-repack"
   :license {:name "The MIT License"
